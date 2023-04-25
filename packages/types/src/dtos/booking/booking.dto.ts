7498604b9d226fdf6352674893998d29194d660a
KenshiroKosuke <mahasamud555@gmail.com>
2023-04-25 16:43:20 +0700

    fix: fix type of DTOs

--- packages/types/src/dtos/booking/booking.dto.ts
+++ packages/types/src/dtos/booking/booking.dto.ts
@@ -52,5 +52,5 @@ export class Booking {
   })
   @IsString()
   @IsNotEmpty()
-  companyId: String;
+  companyId: string;
 }
--- packages/types/src/dtos/company/company.dto.ts
+++ packages/types/src/dtos/company/company.dto.ts
@@ -5,19 +5,19 @@ import { IsNotEmpty, IsPhoneNumber, IsString, Validate } from "class-validator";
 class Address {
   @IsString()
   @IsNotEmpty()
-  street: String;
+  street: string;
 
   @IsString()
   @IsNotEmpty()
-  city: String;
+  city: string;
 
   @IsString()
   @IsNotEmpty()
-  state: String;
+  state: string;
 
   @IsString()
   @IsNotEmpty()
-  zip: String;
+  zip: string;
 }
 
 export class Company {
@@ -29,7 +29,7 @@ export class Company {
   })
   @IsString()
   @IsNotEmpty()
-  id: String;
+  id: string;
 
   @ApiProperty({
     type: () => String,
@@ -39,21 +39,21 @@ export class Company {
   })
   @IsString()
   @IsNotEmpty()
-  name: String;
+  name: string;
 
   @Type(() => Address)
   address: Address;
 
   @IsString()
   @IsNotEmpty()
-  website: String;
+  website: string;
 
   @IsString()
   @IsNotEmpty()
-  description: String;
+  description: string;
 
   @IsString()
   @IsNotEmpty()
   @IsPhoneNumber("TH")
-  tel: String;
+  tel: string;
 }
