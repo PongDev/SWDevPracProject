5bf56dc3d4876cc8eaca45fe6f036daa73ae2945
PongDev <pongwiwat.dev@gmail.com>
2023-04-26 08:33:52 +0700

    ci: add github workflows

--- apps/frontend/next.config.js
+++ apps/frontend/next.config.js
@@ -1,6 +1,11 @@
+const frontendConfig = require("config").frontendConfig;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    backendBaseURL: frontendConfig.backendBaseURL,
+  },
 };
 
 module.exports = nextConfig;
