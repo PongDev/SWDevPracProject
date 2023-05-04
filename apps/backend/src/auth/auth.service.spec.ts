72f014b26f27e79fe35847b94e9203f55c0a10de
PongDev <pongwiwat.dev@gmail.com>
2023-05-04 16:23:47 +0700

    ci: add run tests

--- apps/backend/src/auth/auth.service.spec.ts
+++ apps/backend/src/auth/auth.service.spec.ts
@@ -1,12 +1,17 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './jwt.strategy';
+import { JwtRefreshTokenStrategy } from './jwt-refresh.strategy';
 
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      imports: [PassportModule, JwtModule.register({})],
+      providers: [AuthService, JwtStrategy, JwtRefreshTokenStrategy],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
