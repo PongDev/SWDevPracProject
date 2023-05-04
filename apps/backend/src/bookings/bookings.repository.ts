1587c64780b0ba51c5bbd44d3f0a5d9626ead39e
KenshiroKosuke <mahasamud555@gmail.com>
2023-05-05 05:34:09 +0700

    feat: add booking module

---
+++ apps/backend/src/bookings/bookings.repository.ts
@@ -0,0 +1,100 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { Booking } from 'database';
+import { EditBookingRequest } from 'types';
+import { InvalidRequestError, RecordNotFound } from 'src/common/commonError';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
+
+@Injectable()
+export class BookingsRepository {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async create(createBookingData: {
+    startDate: Date;
+    endDate: Date;
+    userId: number;
+    companyId: number;
+  }): Promise<Booking> {
+    if (
+      createBookingData.startDate.toISOString >
+      createBookingData.endDate.toISOString
+    ) {
+      throw new InvalidRequestError(
+        'Please enter valid dates for an interview session.',
+      );
+    }
+    return await this.prismaService.booking.create({
+      data: createBookingData,
+    });
+  }
+
+  async find(): Promise<Booking[]> {
+    return await this.prismaService.booking.findMany();
+  }
+
+  async findUnique(bookingId: number): Promise<Booking> {
+    const result = await this.prismaService.booking.findUniqueOrThrow({
+      where: {
+        id: bookingId,
+      },
+    });
+    if (!result)
+      throw new RecordNotFound(`Booking with ${bookingId} doesn't exist`);
+    return result;
+  }
+
+  async findByUserId(userId: number) {
+    return await this.prismaService.booking.findMany({
+      where: {
+        userId: userId,
+      },
+    });
+  }
+
+  async update(
+    bookingId: number,
+    bookingUpdateData: EditBookingRequest,
+  ): Promise<Booking> {
+    const target = await this.prismaService.booking.findUnique({
+      where: {
+        id: bookingId,
+      },
+    });
+    if (!target)
+      throw new RecordNotFound(`Booking id${bookingId} doesn't exist.`);
+    if (bookingUpdateData.startDate)
+      target.startDate = bookingUpdateData.startDate;
+    if (bookingUpdateData.endDate) target.endDate = bookingUpdateData.endDate;
+    if (target.startDate.toISOString > target.endDate.toISOString) {
+      throw new InvalidRequestError(
+        'Please enter valid dates for an interview session.',
+      );
+    }
+    return await this.prismaService.booking.update({
+      data: bookingUpdateData,
+      where: {
+        id: bookingId,
+      },
+    });
+  }
+
+  async remove(bookingId: number): Promise<Booking> {
+    try {
+      const result = await this.prismaService.booking.delete({
+        where: {
+          id: bookingId,
+        },
+      });
+      if (!result)
+        throw new RecordNotFound(`Booking id${bookingId} doesn't exist.`);
+      return result;
+    } catch (error) {
+      if (
+        error instanceof PrismaClientKnownRequestError &&
+        error.code === 'P2025'
+      )
+        throw new RecordNotFound(`Booking id${bookingId} doesn't exist.`);
+      throw error;
+    }
+  }
+}
---
+++ apps/backend/src/bookings/bookings.service.ts
@@ -0,0 +1,36 @@
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { CreateBookingRequest, EditBookingRequest } from 'types';
+import { BookingsRepository } from './bookings.repository';
+import { UsersService } from 'src/users/users.service';
+@Injectable()
+export class BookingsService {
+  constructor(private readonly bookingsRepo: BookingsRepository) {}
+
+  async create(createBookingDto: CreateBookingRequest) {
+    return await this.bookingsRepo.create(createBookingDto);
+  }
+
+  async findAll() {
+    return await this.bookingsRepo.find();
+  }
+
+  async findUnique(bookingId: number) {
+    return await this.bookingsRepo.findUnique(bookingId);
+  }
+
+  async findByUserId(userId: number) {
+    return await this.bookingsRepo.findByUserId(userId);
+  }
+
+  async update(bookingId: number, editBookingDto: EditBookingRequest) {
+    return await this.bookingsRepo.update(bookingId, editBookingDto);
+  }
+
+  async remove(bookingId: number) {
+    return await this.bookingsRepo.remove(bookingId);
+  }
+}
