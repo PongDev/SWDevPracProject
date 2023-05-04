b700163a8f8d30b90c771aa28b4884823eb1f2e3
KenshiroKosuke <mahasamud555@gmail.com>
2023-05-04 09:57:44 +0700

    fix: remove auth module dependency

--- apps/backend/src/users/users.module.ts
+++ apps/backend/src/users/users.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
-import { AuthModule } from 'src/auth/auth.module';
 import { UsersRepository } from './users.repository';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 
 @Module({
-  imports: [AuthModule],
   controllers: [UsersController],
   providers: [UsersService, UsersRepository, PrismaService],
   exports: [UsersService],
