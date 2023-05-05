eccd5e27206ea77c491c601e27bbd3af8266038a
KenshiroKosuke <mahasamud555@gmail.com>
2023-05-05 16:25:50 +0700

    fix: update auth test and controller error

--- apps/backend/src/auth/auth.e2e.spec.ts
+++ apps/backend/src/auth/auth.e2e.spec.ts
@@ -9,7 +9,7 @@ import { CreateUserRequest } from 'types';
 import * as request from 'supertest';
 import { AuthModule } from './auth.module';
 
-describe('UsersController', () => {
+describe('AuthModule (e2e)', () => {
   let app: INestApplication;
   const userData = {
     1: {
@@ -31,6 +31,8 @@ describe('UsersController', () => {
   };
   const mockUsersService = {
     register: (req: CreateUserRequest): User => {
+      console.log(req);
+      console.log('..............');
       const { password, ...data } = req;
       const hashedPassword = bcrypt.hashSync(password, 10);
       if (Object.values(userData).some((user) => user.email === data.email)) {
@@ -92,13 +94,145 @@ describe('UsersController', () => {
     describe('missing field', () => {
       it('should return 400', () => {
         return request(app.getHttpServer())
-          .post('/auth/login')
+          .post('/auth/register')
           .send({
+            name: 'Test Register',
             email: 'john@gmail.com',
+            password: '33333',
+          })
+          .expect(HttpStatus.BAD_REQUEST);
+      });
+    });
+    describe('invalid email', () => {
+      it('should return 400', () => {
+        return request(app.getHttpServer())
+          .post('/auth/register')
+          .send({
+            name: 'Test Register',
+            email: 'johngmail.com',
             password: '33333',
             tel: '0888888888',
           })
           .expect(HttpStatus.BAD_REQUEST);
+      });
+    });
+    describe('invalid phone number', () => {
+      it('should return 400', () => {
+        return request(app.getHttpServer())
+          .post('/auth/register')
+          .send({
+            name: 'Test Register',
+            email: 'john@gmail.com',
+            password: '33333',
+            tel: '077',
+          })
+          .expect(HttpStatus.BAD_REQUEST);
+      });
+    });
+    describe('all field presented', () => {
+      it('should return 201', () => {
+        return request(app.getHttpServer())
+          .post('/auth/register')
+          .send({
+            name: 'Test Register',
+            email: 'john2@gmail.com',
+            password: '33333',
+            tel: '0888888888',
+          })
+          .expect(HttpStatus.CREATED);
+      });
+    });
+  });
+
+  describe('login user', () => {
+    describe('missing password', () => {
+      it('should return 400', () => {
+        return request(app.getHttpServer())
+          .post('/auth/login')
+          .send({
+            email: 'test1@gmail.com',
+          })
+          .expect(HttpStatus.BAD_REQUEST);
+      });
+    });
+    describe('missing email', () => {
+      it('should return 400', () => {
+        return request(app.getHttpServer())
+          .post('/auth/login')
+          .send({
+            password: '12345',
+          })
+          .expect(HttpStatus.BAD_REQUEST);
+      });
+    });
+    describe('invalid email', () => {
+      it('should return 400', () => {
+        return request(app.getHttpServer())
+          .post('/auth/login')
+          .send({
+            email: 'test1@gmail',
+            password: '12345',
+          })
+          .expect(HttpStatus.BAD_REQUEST);
+      });
+    });
+    describe('incorrect password', () => {
+      it('should return 401', () => {
+        return request(app.getHttpServer())
+          .post('/auth/login')
+          .send({
+            email: 'test1@gmail.com',
+            password: '000000',
+          })
+          .expect(HttpStatus.UNAUTHORIZED);
+      });
+    });
+    describe('all field presented', () => {
+      it('should return 200', () => {
+        return request(app.getHttpServer())
+          .post('/auth/login')
+          .send({
+            email: 'test1@gmail.com',
+            password: '12345',
+          })
+          .expect(HttpStatus.OK);
+      });
+    });
+  });
+
+  describe('refresh', () => {
+    describe('invalid refresh token', () => {
+      it('should return 401', async () => {
+        // login to get refreshToken
+        const res = await request(app.getHttpServer())
+          .post('/auth/login')
+          .send({
+            email: 'test1@gmail.com',
+            password: '12345',
+          });
+        console.log(res.body);
+        // user refreshToken
+        const res2 = await request(app.getHttpServer())
+          .post('/auth/refresh')
+          .set('Authorization', `Bearer ${res.body.refreshToken}0`);
+        expect(res2.statusCode).toBe(HttpStatus.UNAUTHORIZED);
+      });
+    });
+    describe('valid refresh token', () => {
+      it('should return 200', async () => {
+        // login to get refreshToken
+        const res = await request(app.getHttpServer())
+          .post('/auth/login')
+          .send({
+            email: 'test1@gmail.com',
+            password: '12345',
+          });
+        console.log(res.body);
+        // user refreshToken
+        const res2 = await request(app.getHttpServer())
+          .post('/auth/refresh')
+          .set('Authorization', `Bearer ${res.body.refreshToken}`);
+        expect(res2.statusCode).toBe(HttpStatus.OK);
       });
     });
   });
